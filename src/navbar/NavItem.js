3a8b05dff59a1ebb0034a781d3a34e87506d335c
Ewe Tek Min <ewe.tekmin@outlook.com>
2019-11-27 00:06:42 +0800

    Add navbar

---
+++ src/navbar/NavItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledNavItem = styled.a`
+  height: 80px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  padding: 0 1rem;
+
+  :hover {
+    border-bottom: 1px solid black;
+  }
+`
+
+const NavItem = ({ children }) => {
+  return (
+    <StyledNavItem>
+      <a>
+        {children}
+      </a>
+    </StyledNavItem>
+  )
+}
+
+export default NavItem;
--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import NavItem from './NavItem';
+
 const StyledNavbar = styled.nav`
-  list-style-type: none;
   margin: 0;
   padding: 0;
+  width: 100%;
+  height: 81px;
+  border-bottom: 1px solid #e0e0e0;
+  display: flex;
+  justify-content: space-between;
 `
 
-const Navbar = () => {
+const Navbar = ({ children }) => {
   return (
     <StyledNavbar>
+      {children}
     </StyledNavbar>
   )
 }
 
-export const Item = () => {
-  return (
-    <StyledNavbar>
-    </StyledNavbar>
-  )
-}
+Navbar.Item = NavItem;
 
 export default Navbar;
