72168bad3578e6b71a0d8332348a70a73805ae4e
Ewe Tek Min <ewe.tekmin@outlook.com>
2019-11-26 08:05:56 +0800

    Create dummy navbar component

---
+++ src/navbar/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledNavbar = styled.nav`
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+`
+
+const Navbar = () => {
+  return (
+    <StyledNavbar>
+    </StyledNavbar>
+  )
+}
+
+export const Item = () => {
+  return (
+    <StyledNavbar>
+    </StyledNavbar>
+  )
+}
+
+export default Navbar;
