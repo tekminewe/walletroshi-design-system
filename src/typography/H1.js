d5cbfb93a3bfcf4e591fa7353b4857aef1b2c64d
Ewe Tek Min <ewe.tekmin@outlook.com>
2019-11-29 18:14:54 +0800

    Update text size

--- src/typography/H1.js
+++ src/typography/H1.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const StyledH1 = styled.h1`
-
-`
+const StyledH1 = styled.h1``
 
 const H1 = ({ children, ...props }) => (
   <StyledH1 {...props}>{children}</StyledH1>
