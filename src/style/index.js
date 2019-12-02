bdb75c44f6a88f99d5722dd3bc4fdc8807a96bc2
Ewe Tek Min <ewe.tekmin@outlook.com>
2019-12-02 21:58:01 +0800

    Change to Quicksand font

--- src/style/index.js
+++ src/style/index.js
@@ -2,7 +2,7 @@ import { createGlobalStyle, css } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
   body {
-    font-family: 'Abel', sans-serif;
+    font-family: 'Quicksand', sans-serif;
     margin: 0;
     padding: 0;
   }
