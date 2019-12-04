01aca32c55285f9aa5365310a3ae7cd1bca9cba3
Ewe Tek Min <ewe.tekmin@outlook.com>
2019-12-04 20:42:20 +0800

    Add Spacer

---
+++ src/layout/Spacer.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const StyledSpacer = styled.div`
+  height: 1em;
+`
+
+export default StyledSpacer;
--- src/layout/index.js
+++ src/layout/index.js
@@ -1,9 +1,11 @@
 import Center from './Center';
 import Column from './Column';
 import Row from './Row';
+import Spacer from './Spacer';
 
 export {
   Center,
   Column,
   Row,
+  Spacer,
 }
