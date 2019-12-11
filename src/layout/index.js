ddeb127bda0fdb2e23f69fc195d69c323fba0b10
Ewe Tek Min <ewe.tekmin@outlook.com>
2019-12-11 23:18:54 +0800

    Add separator

--- src/layout/index.js
+++ src/layout/index.js
@@ -2,10 +2,12 @@ import Center from './Center';
 import Column from './Column';
 import Row from './Row';
 import Spacer from './Spacer';
+import Separator from './Separator';
 
 export {
   Center,
   Column,
   Row,
   Spacer,
+  Separator,
 }
